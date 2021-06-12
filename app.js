6fc316d369af19f3910a2635bb19db7f318b0f36
izebel <izebel@rocketmail.com>
2021-06-12 14:03:22 -0400

    change to gmail host and port

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.listen(PORT, () => {
 });
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.mail.yahoo.com",
-    port: 465,
+    host: "smtp.gmail.com",
+    port: 587,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASS,
